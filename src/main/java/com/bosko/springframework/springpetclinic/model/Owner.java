dd9b01142da2d6c17af9fed4f100f99803ffc209
boskodjokic <boskodjokic@yahoo.com>
2021-04-14 11:56:47 +0200

    adding POJOs

---
+++ src/main/java/com/bosko/springframework/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.bosko.springframework.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/bosko/springframework/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.bosko.springframework.springpetclinic.model;
+
+public class Vet extends Person {
+}
