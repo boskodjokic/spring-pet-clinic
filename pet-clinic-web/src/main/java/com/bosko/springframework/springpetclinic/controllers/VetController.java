441c42eb60295ac9f707ab1d56055ce4bca835af
boskodjokic <boskodjokic@yahoo.com>
2021-04-15 10:51:19 +0200

    Added Owners and Vets list and updated controller.

--- pet-clinic-web/src/main/java/com/bosko/springframework/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/bosko/springframework/springpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.bosko.springframework.springpetclinic.controllers;
 
+import com.bosko.springframework.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
