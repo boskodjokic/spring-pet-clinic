aa8cccb7beb48c2ff972bfa3c46848896b3e7b93
boskodjokic <boskodjokic@yahoo.com>
2021-04-18 20:03:33 +0200

    Added PetTypeService.

---
+++ pet-clinic-data/src/main/java/com/bosko/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.bosko.springframework.springpetclinic.services;
+
+import com.bosko.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
