43903d9aa945cf6f7f0ed8338416d114dde732f2
boskodjokic <boskodjokic@yahoo.com>
2021-04-18 20:35:14 +0200

    Added Specialties Service.

---
+++ pet-clinic-data/src/main/java/com/bosko/springframework/springpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.bosko.springframework.springpetclinic.services;
+
+import com.bosko.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
