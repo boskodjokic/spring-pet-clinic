cd21c0f9039aaf6c95053d4a899f24e54ab00f82
boskodjokic <boskodjokic@yahoo.com>
2021-04-18 21:50:08 +0200

    Added notImplemented page.

--- pet-clinic-web/src/main/java/com/bosko/springframework/springpetclinic/controllers/OwnerControllers.java
+++ pet-clinic-web/src/main/java/com/bosko/springframework/springpetclinic/controllers/OwnerControllers.java
@@ -15,9 +15,15 @@ public class OwnerControllers {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html",})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+
+        return "notImplemented";
     }
 }
