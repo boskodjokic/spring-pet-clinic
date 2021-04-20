8263df2bd68b1c37c63d7266ca80755af5f2486d
boskodjokic <boskodjokic@yahoo.com>
2021-04-20 10:01:53 +0200

    Added Map impl for visit service.

---
+++ pet-clinic-data/src/main/java/com/bosko/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.bosko.springframework.springpetclinic.services;
+
+import com.bosko.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
