94b98033c7ac6be96df282bb1e5c993e245d843b
boskodjokic <boskodjokic@yahoo.com>
2021-04-21 12:19:27 +0200

    Added OwnerControllerTest.

--- pet-clinic-web/src/main/java/com/bosko/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/bosko/springframework/springpetclinic/controllers/OwnerController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
-public class OwnerControllers {
+public class OwnerController {
 
     private final OwnerService ownerService;
 
-    public OwnerControllers(OwnerService ownerService) {
+    public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
