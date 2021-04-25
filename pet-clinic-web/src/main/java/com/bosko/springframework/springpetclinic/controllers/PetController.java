2163965f6dfafaacc370f0aea76d8dd8d5c6474e
boskodjokic <boskodjokic@yahoo.com>
2021-04-25 19:44:32 +0200

    Adding model attributes for Pet Controller.

---
+++ pet-clinic-web/src/main/java/com/bosko/springframework/springpetclinic/controllers/PetController.java
@@ -0,0 +1,47 @@
+package com.bosko.springframework.springpetclinic.controllers;
+
+import com.bosko.springframework.springpetclinic.model.Owner;
+import com.bosko.springframework.springpetclinic.model.PetType;
+import com.bosko.springframework.springpetclinic.services.OwnerService;
+import com.bosko.springframework.springpetclinic.services.PetService;
+import com.bosko.springframework.springpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owenrs/{ownerId}")
+public class PetController {
+
+    public static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+ }
