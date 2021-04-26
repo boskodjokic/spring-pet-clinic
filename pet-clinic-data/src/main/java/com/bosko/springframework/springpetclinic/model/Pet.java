8edbbeab933a7d5c50f0930c2c873bd6812ed5b4
boskodjokic <boskodjokic@yahoo.com>
2021-04-26 16:25:43 +0200

    Adding list view for vets.

--- pet-clinic-data/src/main/java/com/bosko/springframework/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/bosko/springframework/springpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.bosko.springframework.springpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -40,9 +41,9 @@ public class Pet extends BaseEntity{
     private Owner owner;
 
     @Column(name="birthdate")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
-
 }
